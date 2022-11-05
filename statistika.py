933d55f68185380f3f8f5cf59d39e0cedc019adc
afr00x <109451961+afr00x@users.noreply.github.com>
2022-11-05 20:18:39 +0700

    Create statistika.py

---
+++ statistika.py
@@ -0,0 +1,100 @@
+import os
+from time import sleep
+
+
+print ("Statistika Matjib")
+
+sleep (1)
+os.system ('cls')
+
+print ("""
+    ⣴⣶⢶⣶⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣶⣶⣶⣶⡄
+⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⡇
+⠙⠋⠻⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣤⡄⠀⠀⠀⠀⠀⣴⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⡿⠋⠙⠁
+⠀⠀⠀⠈⢿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⣻⣿⣿⡟⠛⠀⠛⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⠟⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠙⢿⣿⣿⣷⣄⠀⠀⠀⠀⣰⣿⡿⠿⠟⠀⠀⠀⠛⠻⣿⣷⣄⠀⠀⠀⢀⣴⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣷⣄⣤⣾⡿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣷⣤⣴⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⡿⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢘⡿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⡿⠻⣷⡀⠑⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⠋⣠⡾⠛⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢠⣶⣦⣠⣴⣾⣿⠟⠉⠙⢆⠈⠻⡦⡈⠳⡄⠀⠀⠀⠀⠀⣠⠊⢠⣾⠟⢀⠜⠉⠉⢻⣿⣶⣤⢤⣶⣦⠀⠀⠀
+⠀⠀⠈⢿⣿⣬⣿⠋⠋⠀⠀⠀⠀⠑⢄⠙⢾⢦⠈⢢⡀⠀⢠⠞⢁⣴⠟⢁⡴⠋⠀⠀⠀⠈⠛⢻⣿⣽⣿⠛⠀⠀⠀
+⠀⠀⠀⠸⠏⠉⠋⠀⠀⠀⠀⠀⠀⠀⠈⠳⣄⠑⢕⢄⢙⡖⠁⣰⡟⠁⡠⠊⠀⠀⠀⠀⠀⠀⠀⠈⠋⠁⠻⠂⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠢⣀⡵⠋⡠⡾⠋⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⢠⠾⠋⢀⠞⠳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠜⢁⠔⠃⢀⣴⠉⢢⣀⠈⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠃⡰⠋⢀⡴⠋⠀⠑⢄⠙⢥⣄⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠊⡠⠎⠁⡠⠋⠀⠀⠀⠀⠀⠳⣄⠙⢧⡈⠳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠁⠀⠁⣠⠊⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⡄⠐⠆⠘⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⠃⠀⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠢⡀⠀⠀⠱⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⠞⠁⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠢⡀⠀⠈⢢⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⡰⠃⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠢⡀⠀⠑⣄⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⠜⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠢⡀⠈⢦⠀⠀⠀⠀⠀
+⠀⠀⠀⢠⣏⡤⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⢄⣣⡀⠀⠀⠀
+⠀⠀⠀⠙⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀""")
+
+sleep (5)
+os.system ('cls')
+
+def menu():
+    print ("1.Median/Nilai Tengah")
+    print ("2.Modus/Nilai Yang Sering Muncul")
+    print ("3.Cara Pemakaian")
+    print ("0.Exit The Program")
+    print ("""
+    ============================
+        A F R A  C O D E S
+    ============================
+     """)
+
+menu()
+a = float (input("Tentukan Pilihan : "))
+
+os.system('cls')
+while a != 0:
+    if a == 1: 
+       n = float (input("Jumlah Frekuensi: "))
+       kel_med = 0.5 * (n + 1)
+       text_kel = F"Kelas Median : {kel_med}"
+       print (text_kel)
+
+       tepi_bawah = float (input("Masukkan Tepi Bawah Kelas Median: "))
+       frekuensi_kumulatif = float (input("Masukkan Frekuensi Sebelum Kelas Median: "))
+       frekuensi_kelas = float (input("Masukkan Frekuensi Kelas Median: "))
+       panjang_kelas = float (input("Masukkan Panjang Kelas dari Kelas Median : "))
+
+       nilai_n = 0.5 * n
+       median1 = nilai_n - frekuensi_kumulatif 
+       median1_1 = median1 / frekuensi_kelas
+       median2 = median1_1 * panjang_kelas
+       median3 = median2 + tepi_bawah
+       text_median = F"Median/Nilai Tengah: {median3}"
+       print (text_median)
+       
+    elif a == 2:
+        tepbaw = float (input("Masukkan Tepi Bawah: "))
+        d1 = float (input("Masukkan Frekuensi Sebelum Kelas Modus : "))
+        d2 = float (input("Masukkan Frekuensi Setelah Kelas Modus : "))
+        pankel = float (input("Masukkan Panjang Kelas : "))
+
+        modus0_1 = d1 + d2
+        modus0_2 = d1 / modus0_1
+        modus0_3 = modus0_2 * pankel
+        modus = tepbaw + modus0_3
+        text_modus = f"Nilai Modus : {modus} "
+        print (text_modus)
+        
+    elif a == 3:
+        print ("""
+        ===========================================
+        Cara Penggunaan:
+        1. Gunakan Angka
+        2. Untuk Angka Berkoma Gunakan Tanda TITIK
+        3. Contoh : 167.5
+        ===========================================
+        """)
+        pass
+    sleep(5)
+    os.system ('cls')
+    print()        
+    menu()
+    a = float(input("Tentukan Pilihan: "))
